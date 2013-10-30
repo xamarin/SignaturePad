890f966b00c857842d8fb5aa152bb85fac20e3ae
Arturo Espinosa <arturo.espinosa@xamarin.com>
2013-10-29 23:21:22 -0600

    Modified the sample with more customizations to illustrate more flexibility of the view.
    Also added an example of the new background image with a watermark.

--- samples/Sample.iOS/Controllers/SampleController.cs
+++ samples/Sample.iOS/Controllers/SampleController.cs
@@ -33,9 +33,22 @@ namespace Sample {
 				UIApplication.SharedApplication.StatusBarStyle = UIStatusBarStyle.LightContent;
 			}
 
-			// Customize the font.
-			view.Signature.Caption.Text = "Authorization Signature";
-			view.Signature.Caption.Font = UIFont.FromName ("Marker Felt", 16f);
+			if (true) { // Customization activated.
+				view.Signature.Caption.Text = "Authorization Signature";
+				view.Signature.Caption.Font = UIFont.FromName ("Marker Felt", 16f);
+				view.Signature.SignaturePrompt.Text = "☛";
+				view.Signature.SignaturePrompt.Font = UIFont.FromName ("Helvetica", 32f);
+				view.Signature.BackgroundColor = UIColor.FromRGB (255, 255, 200); // a light yellow.
+
+				view.Signature.BackgroundImageView.Image = UIImage.FromBundle ("logo-galaxy-black-64.png");
+				view.Signature.BackgroundImageView.Alpha = 0.0625f;
+				view.Signature.BackgroundImageView.ContentMode = UIViewContentMode.ScaleToFill;
+				view.Signature.BackgroundImageView.Frame = new System.Drawing.RectangleF (20, 20, 256, 256);
+
+				// Modify shadow
+				view.Signature.Layer.ShadowOffset = new System.Drawing.SizeF (0, 0);
+				view.Signature.Layer.ShadowOpacity = 1f;
+			}
 		}
 	}
 }
