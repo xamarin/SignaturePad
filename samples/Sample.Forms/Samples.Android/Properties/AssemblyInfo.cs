1177f26917485bc74bb9160f27809b3a9ee98dbe
Matthew Leibowitz <mattleibow@live.com>
2016-05-31 23:33:23 +0200

    fixed the forms sample

--- samples/Sample.Forms/Samples.Android/Properties/AssemblyInfo.cs
+++ samples/Sample.Forms/Samples.Android/Properties/AssemblyInfo.cs
@@ -13,16 +13,3 @@ using Android.App;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessMockLocation)]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)]
-[assembly: UsesPermission(Android.Manifest.Permission.CallPhone)]
-[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
-[assembly: UsesPermission(Android.Manifest.Permission.Internet)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadContacts)]
-[assembly: UsesPermission(Android.Manifest.Permission.ReadPhoneState)]
-[assembly: UsesPermission(Android.Manifest.Permission.SendSms)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
-[assembly: UsesPermission(Android.Manifest.Permission.WriteContacts)]
