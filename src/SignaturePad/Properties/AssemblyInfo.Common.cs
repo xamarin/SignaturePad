2ea66bbf71dad77b6ba1d5b9e12a273c516d3089
Matthew Leibowitz <mattleibow@live.com>
2017-03-30 00:57:33 +0200

    Upped the version numbers because we want to release

--- src/SignaturePad/Properties/AssemblyInfo.Common.cs
+++ src/SignaturePad/Properties/AssemblyInfo.Common.cs
@@ -16,5 +16,5 @@ using System.Resources;
 [assembly: NeutralResourcesLanguageAttribute ("en-US")]
 
 [assembly: AssemblyVersion ("1.4.0.0")]
-[assembly: AssemblyFileVersion ("1.4.0.0")]
-[assembly: AssemblyInformationalVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion ("1.4.1.0")]
+[assembly: AssemblyInformationalVersion("1.4.1.0")]
