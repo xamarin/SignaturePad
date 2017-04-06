978680c344e6bec7cb00edc2069b8637abd4230d
Matthew Leibowitz <mattleibow@live.com>
2017-04-06 05:39:02 +0200

    tweaked the iOS sample

--- samples/Sample.iOS/AppDelegate.cs
+++ samples/Sample.iOS/AppDelegate.cs
@@ -1,25 +1,16 @@
-//
-// AppDelegate.cs
-//
-// Author:
-//   Timothy Risi (timothy.risi@gmail.com)
-//
-// Copyright (C) 2016 Xamarin Inc.
-//
-
 using Foundation;
 using UIKit;
 
 namespace Sample.iOS
 {
-    [Register("AppDelegate")]
-    public class AppDelegate : UIApplicationDelegate
-    {
-        public override UIWindow Window { get; set; }
+	[Register ("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate
+	{
+		public override UIWindow Window { get; set; }
 
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
-            return true;
-        }
-    }
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			return true;
+		}
+	}
 }
