e831a6ff74520a9330f30993854f9b7a96afd68c
Matthew Leibowitz <mattleibow@live.com>
2018-05-18 01:29:39 +0200

    whitespaces

--- samples/Sample.Forms/Samples/MainPage.xaml.cs
+++ samples/Sample.Forms/Samples/MainPage.xaml.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
 
 namespace Samples
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : TabbedPage
-    {
-        public MainPage ()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
 }
