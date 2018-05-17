57e495e9cec0de5e087eae76e7a7a6a7dabae92b
Matthew Leibowitz <mattleibow@live.com>
2018-05-18 01:12:05 +0200

    Made the Xamarin.Forms sample bindable using behaviors

---
+++ samples/Sample.Forms/Samples/Helpers/CaptureSignaturePointsBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+using SignaturePad.Forms;
+
+namespace Samples
+{
+	public class CaptureSignaturePointsBehavior : CaptureSignatureBehaviorBase
+	{
+		public static readonly BindableProperty PointsProperty = BindableProperty.Create (
+			nameof (Points),
+			typeof (IEnumerable<Point>),
+			typeof (CaptureSignaturePointsBehavior),
+			default (IEnumerable<Point>),
+			BindingMode.TwoWay,
+			propertyChanged: CreatePropertyChanged (PointsProperty));
+
+		public IEnumerable<Point> Points
+		{
+			get => (IEnumerable<Point>)GetValue (PointsProperty);
+			set => SetValue (PointsProperty, value);
+		}
+
+		protected override void UpdateSignaturePad (SignaturePadView bindable, BindableProperty property, object oldValue, object newValue)
+		{
+			bindable.Points = newValue as IEnumerable<Point>;
+		}
+
+		protected override void UpdateBehavior (SignaturePadView signaturePad)
+		{
+			Points = signaturePad.Points;
+		}
+	}
+}
--- samples/Sample.Forms/Samples/MainPage.xaml.cs
+++ samples/Sample.Forms/Samples/MainPage.xaml.cs
@@ -1,59 +1,14 @@
-﻿using System;
-using System.Linq;
-using Xamarin.Forms;
-
-using SignaturePad.Forms;
+﻿using Xamarin.Forms;
 
 namespace Samples
 {
 	public partial class MainPage : ContentPage
 	{
-		private Point[] points;
-
 		public MainPage ()
 		{
 			InitializeComponent ();
 
-			UpdateControls ();
-		}
-
-		private void UpdateControls ()
-		{
-			btnSave.IsEnabled = !signatureView.IsBlank;
-			btnSaveImage.IsEnabled = !signatureView.IsBlank;
-			btnLoad.IsEnabled = points != null;
-		}
-
-		private void SaveVectorClicked (object sender, EventArgs e)
-		{
-			points = signatureView.Points.ToArray ();
-			UpdateControls ();
-
-			DisplayAlert ("Signature Pad", "Vector signature saved to memory.", "OK");
-		}
-
-		private void LoadVectorClicked (object sender, EventArgs e)
-		{
-			signatureView.Points = points;
-		}
-
-		private async void SaveImageClicked (object sender, EventArgs e)
-		{
-			bool saved;
-			using (var bitmap = await signatureView.GetImageStreamAsync (SignatureImageFormat.Png, Color.Black, Color.White, 1f))
-			{
-				saved = await App.SaveSignature (bitmap, "signature.png");
-			}
-
-			if (saved)
-				await DisplayAlert ("Signature Pad", "Raster signature saved to the photo library.", "OK");
-			else
-				await DisplayAlert ("Signature Pad", "There was an error saving the signature.", "OK");
-		}
-
-		private void SignatureChanged (object sender, EventArgs e)
-		{
-			UpdateControls ();
+			BindingContext = new MainViewModel (signaturePadView.GetImageStreamAsync);
 		}
 	}
 }
