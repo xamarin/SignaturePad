ed58feb45796c0f87349f1521718c564f438d534
Vladislav Antonyuk <vlad.antonyuk@gmail.com>
2018-06-01 17:15:20 +0300

    samples wpf

---
+++ samples/Sample.Forms/Samples.WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿// <copyright file="MainWindow.xaml.cs" company="Vladislav Antonyuk">
+//     Vladislav Antonyuk. All rights reserved.
+// </copyright>
+// <author>Vladislav Antonyuk</author>
+
+namespace Samples.WPF
+{
+	using System;
+	using System.IO;
+	using System.Threading.Tasks;
+	using Xamarin.Forms;
+	using Xamarin.Forms.Platform.WPF;
+
+	/// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Forms.Init();
+            LoadApplication(new Samples.App(OnSaveSignature));
+        }
+
+	    private async Task<bool> OnSaveSignature (Stream bitmap, string filename)
+	    {
+		    var storageFolder = Environment.SpecialFolder.CommonPictures.ToString();
+		    var file = System.IO.File.Create (Path.Combine(storageFolder, filename));
+
+		    using (var dest = file)
+		    {
+			    await bitmap.CopyToAsync (dest);
+		    }
+
+		    return true;
+	    }
+	}
+}
